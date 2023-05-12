0bdfdbfe58cefe24be4a635d591e3a2f2ae9d633
suin <l3esde67@naver.com>
2023-05-12 17:29:19 +0900

    project 생성

---
+++ mes/src/main/java/com/mes/aone/AoneApplication.java
@@ -0,0 +1,13 @@
+package com.mes.aone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoneApplication.class, args);
+    }
+
+}
---
+++ mes/src/test/java/com/mes/aone/AoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mes.aone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
