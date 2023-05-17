6765cc37fc3ec1f37912cc2c830b4c50fc88bc6a
OkDaGyeong <81674153+OkDaGyeong@users.noreply.github.com>
2023-05-17 16:37:12 +0900

    원자재 관리 페이지 더미데이터

--- mes/src/main/java/com/mes/aone/controller/inventoryController.java
+++ mes/src/main/java/com/mes/aone/controller/inventoryController.java
@@ -3,6 +3,7 @@ package com.mes.aone.controller;
 import com.mes.aone.constant.SalesStatus;
 import com.mes.aone.constant.StockManageState;
 import com.mes.aone.dto.MaterialStorageDTO;
+import com.mes.aone.dto.MaterialStorageNumDTO;
 import com.mes.aone.dto.OrderDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,26 +12,39 @@ import org.springframework.web.bind.annotation.GetMapping;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Controller
 public class inventoryController {
     @GetMapping(value="/inventory1")
     public String inventoryPage1(Model model){
+        //원자재 입출고 현황 (더미)
         List<MaterialStorageDTO> materialStorageDTOList = new ArrayList<>();
 
-        for(int i=1; i<=30; i++ ){
+        String[] materialNames = {"양배추","콜라겐", "흑마늘", "벌꿀", "석류액기스", "매실액기스", "스틱파우치", "파우치","박스"};
+
+        Random random = new Random();
+
+        for (int i = 1; i <= 30; i++) {
             MaterialStorageDTO materialStorageDTO = new MaterialStorageDTO();
-//            materialStorageDTO.setMaterialStorageId(Long.valueOf(i));
-            materialStorageDTO.setMaterialName("양배추"); // 자재명
+            materialStorageDTO.setMaterialName(materialNames[random.nextInt(materialNames.length)]);
             materialStorageDTO.setMStorageState(StockManageState.I);
-            materialStorageDTO.setMaterialQty(1000);
+            materialStorageDTO.setMaterialQty(random.nextInt(1000));
             materialStorageDTO.setMaterialUnit("kg");
             materialStorageDTO.setMStorageDate(LocalDateTime.now());
-
             materialStorageDTOList.add(materialStorageDTO);
         }
-
         model.addAttribute("materialStorageDTOList",materialStorageDTOList);
+
+        //원자재 총 수량(더미)
+        List<MaterialStorageNumDTO> materialStorageNumDTOList = new ArrayList<>();
+        for (int i=0; i<9; i++){
+            MaterialStorageNumDTO materialStorageNumDTO = new MaterialStorageNumDTO();
+            materialStorageNumDTO.setMaterialName(materialNames[i]);
+            materialStorageNumDTO.setMaterialQty(random.nextInt(1000));
+            materialStorageNumDTOList.add(materialStorageNumDTO);
+        }
+        model.addAttribute("materialStorageNumDTOList",materialStorageNumDTOList);
 
         return"pages/inventoryPage1";
     }
